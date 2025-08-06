da5278aa52d98ff7905f37ea68c309fae27b731f
Nahid Khan Shuvo <nahidkhanshuvo7@gmail.com>
2025-08-06 16:10:53 +0600

    no update

--- lib/app/modules/onboarding/views/onboarding_view.dart
+++ lib/app/modules/onboarding/views/onboarding_view.dart
@@ -1,5 +1,4 @@
 import 'package:get/get.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import '../controllers/onboarding_controller.dart';
